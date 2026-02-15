9a17d7c4239c2e696cc10b40a441d087ebbf1426
Patolinotop <103897334+Patolinotop@users.noreply.github.com>
2026-02-15 16:52:29 -0300

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -1,10 +1,12 @@
-# webservice.py
-import os
 import asyncio
 from fastapi import FastAPI
 from bot import start_discord_bot
 
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"ok": True}
 
 @app.get("/health")
 def health():
@@ -12,5 +14,4 @@ def health():
 
 @app.on_event("startup")
 async def startup_event():
-    # roda o bot em background no mesmo processo do web service
     asyncio.create_task(start_discord_bot())
